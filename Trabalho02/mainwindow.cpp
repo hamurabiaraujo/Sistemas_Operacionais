2add0f7f8d17c4f46bb7420212482b5cc44ae939
Hamurabi Araújo <hamurabicefetrn@gmail.com>
2014-09-27 12:29:54 -0300

    Projeto 2

---
+++ Trabalho02/mainwindow.cpp
@@ -0,0 +1,53 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QList>
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+  model = new QStandardItemModel();
+  QStringList headers;
+  headers << tr("Nome") << tr("Status") << tr("PID") << tr("PPID") << tr("Usuário");
+  model->setHorizontalHeaderLabels(headers);
+
+
+// model->setHorizontalHeaderItem( 0, new QStandardItem( "Nome" ) );
+// model->setHorizontalHeaderItem( 1, new QStandardItem( "Status" ) );
+ // model->setHorizontalHeaderItem( 2, new QStandardItem( "PID" ) );
+
+
+    QList<QStandardItem *> listaItem;
+    listaItem << new QStandardItem("firefox");
+    listaItem << new QStandardItem("Parado");
+    listaItem << new QStandardItem("1234");
+    listaItem << new QStandardItem("20");
+    listaItem << new QStandardItem("Ivanovitch");
+
+    model->appendRow(listaItem);
+    listaItem.clear();
+
+    listaItem << new QStandardItem("word");
+    listaItem << new QStandardItem("Executando");
+    listaItem << new QStandardItem("345");
+    listaItem << new QStandardItem("1");
+    listaItem << new QStandardItem("Ivanovitch");
+
+    model->appendRow(listaItem);
+
+    ui->tableView->setModel(model);
+    ui->tableView->setShowGrid(false);
+    ui->tableView->setAlternatingRowColors(true);
+    ui->tableView->verticalHeader()->setVisible(false);
+    ui->tableView->setSortingEnabled(true);
+    ui->tableView->sortByColumn(4, Qt::AscendingOrder);
+
+ }
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
